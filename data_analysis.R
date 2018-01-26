5c79c4cd6f4d1be0be9c92761caaa0eec02145e7
Colby Goettel <colby.goettel@gmail.com>
2018-01-25 22:59:45 -0600

    Reworked question 2

--- data_analysis.R
+++ data_analysis.R
@@ -5,6 +5,8 @@ library(car)
 library(ggplot2)
 library(grid)
 library(Kendall)
+library(lmtest)
+library(sandwich)
 library(stargazer)
 
 ## Input data
@@ -27,18 +29,17 @@ it_ae_ro<-data.mis$AE.RO[data.mis$Major=="it"]
 cs_major_gpa<-data.mis$Major.GPA[data.mis$Major=="cs"]
 it_major_gpa<-data.mis$Major.GPA[data.mis$Major=="it"]
 
-# First, let's see if there's a correlation between CS and IT AC-CE and AE-RO.
-# "The t-test is used to test whether there is a difference between two groups
-# on a continuous dependent variable." That is appropriate for checking if
-# there's a difference between CS and IT majors with their AC-CE and AE-RO
-# values, but not for checking the significance between those values and the
-# relationship to GPA.
+# First, let's see if there's a correlation between CS and IT
+# AC-CE and AE-RO. "The t-test is used to test whether there is
+# a difference between two groups on a continuous dependent
+# variable." That is appropriate for checking if there's a
+# difference between CS and IT majors with their AC-CE and AE-RO
+# values, but not for checking the significance between those
+# values and the relationship to GPA.
 t.test(cs_ac_ce, it_ac_ce) # p = 0.5411
 t.test(cs_ae_ro, it_ae_ro) # p = 0.3501
-# I don't know if I'm doing that right, so just to be safe:
-t.test(cs_ac_ce,cs_ae_ro) # p = 0.3756
-t.test(it_ac_ce,it_ae_ro) # p = 0.5674
-# So there's no relationship between them. And we can see that in the plot:
+# So there's no relationship between them. And we can see that
+# in the plot:
 df<-data.frame(cs_ac_ce,cs_ae_ro)
 df2<-data.frame(it_ac_ce,it_ae_ro)
 cs_v_it_plot<-ggplot(df, aes(cs_ac_ce,cs_ae_ro)) +
@@ -48,16 +49,16 @@ cs_v_it_plot<-cs_v_it_plot + geom_point(data=df2, x=it_ac_ce,
 cs_v_it_plot<-cs_v_it_plot + labs(x="AC-CE", y="AE-RO")
 cs_v_it_plot<-cs_v_it_plot + scale_fill_manual(name="Majors",
   values = c("CS" = "black", "IT" = "red"))
-jpeg('cs-v-it-plot.jpg', width = 1000, height = 1000)
+jpeg('cs-v-it-plot.jpg', width = 500, height = 500)
 cs_v_it_plot
 dev.off()
 
 # Research questions
 ## Question 1: How strong is the correlation between AC-CE and
 ## AE-RO, and college GPA in CS, IS, and IT?
-# Before we know whether to use Pearson's or Spearman's, we have to know if the
-# data is normally distributed. If it is, we use Pearson's; if not, we use
-# Spearman's.
+# Before we know whether to use Pearson's or Spearman's, we have
+# to know if the data is normally distributed. If it is, we use
+# Pearson's; if not, we use Spearman's.
 shapiro.test(cs_major_gpa) # p = 0.148
 shapiro.test(it_major_gpa) # p = 0.8639
 shapiro.test(cs_ac_ce) # p = 0.02512
@@ -69,33 +70,31 @@ cor.test(cs_major_gpa, cs_ac_ce) # p = 0.8177
 cor.test(cs_major_gpa, cs_ae_ro) # p = 0.6704
 cor.test(it_major_gpa, it_ac_ce) # p = 0.9727
 cor.test(it_major_gpa, it_ae_ro) # p = 0.02017, cor = 0.4914967
-# This means that as IT AE-RO increases, there's a 0.4914967 increase in GPA.
+# This means that as IT AE-RO increases, there's a positively
+# related change in GPA which we can see in the Figure.
+
 # Let's get a summary of it_major_gpa so we can see just what that means.
 summary(it_major_gpa)
 #    Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
 #  2.340   2.868   3.200   3.204   3.480   3.970
 sd(it_major_gpa) # sd = 0.4439921
-# CHECK: If I'm reading this right, it's showing an increase of 0.44 GPA per sd.
-# This is huge when it comes to IT students being more AE-RO (meaning they're
-# stronger in the AE bit).
+hist(it_major_gpa) # Almost perfectly normal (p = 0.86)
 
-cor.test(cs_major_gpa, cs_ac_ce, method = "spearman") # p = 0.8232
-cor.test(cs_major_gpa, cs_ae_ro, method = "spearman") # p = 0.8342
-cor.test(it_major_gpa, it_ac_ce, method = "spearman") # p = 0.9262
-cor.test(it_major_gpa, it_ae_ro, method = "spearman") # p = 0.0161, rho = 0.5066
-# Spearman's and Pearson's are showing the same thing which is great news. But,
-# Spearman's freaks out when there's ties, so let's use Kendall's tau-b to
-# check.
+# Should only be used on this first one.
+cor.test(cs_major_gpa, cs_ac_ce, method = "spearman") # p=0.8232
+cor.test(cs_major_gpa, cs_ae_ro, method = "spearman") # p=0.8342
+cor.test(it_major_gpa, it_ac_ce, method = "spearman") # p=0.9262
+cor.test(it_major_gpa, it_ae_ro, method = "spearman") # p=0.0161,
+# rho = 0.5066
+# Spearman's and Pearson's are showing the same thing which is
+# great news. But, Spearman's freaks out when there's ties, so
+# let's use Kendall's tau-b (which handles ties) to check.
 Kendall(cs_major_gpa,cs_ac_ce) # p = 0.71763
 Kendall(cs_major_gpa,cs_ae_ro) # p = 0.82436
 Kendall(it_major_gpa,it_ac_ce) # p = 0.95484
 Kendall(it_major_gpa,it_ae_ro) # p = 0.02154, tau = 0.365
-# And again the same p-value being significant holds. I'm convinced. Well,
-# except for t-tests. Let's actually learn about those.
-# The average of the three correlations:
-#   ( 0.365 + 0.5066691 + 0.4439921 ) / 3
-# is 0.4385537 which is right around what I was writing about earlier on the
-# effect that an IT student's AE-RO has on their GPA.
+# And again the same p-value being significant holds. I'm
+# convinced.
 
 # Let's see what they look like
 # First plot
@@ -142,15 +141,36 @@ dev.off()
 
 ## Question 2: What is the best multiple regression model to fit
 ## these correlations?
+# Let's calculate the robust standard of error and use that
+# throughout these calculations. The assumption is that you will
+# use robust standard of error; if you choose not to, you have
+# to prove why you don't have to.
+# Robust standard error: normal standard error does not handle
+# heteroskedasticity so we need to have the robust standard
+# error. So, "I ran this model with standard errors computed with
+# Huber-White (HC1) robust standard error to account for the
+# heteroskedasticity of the data."
+
 ### Model 1: with AC-CE and AE-RO and a CS dummy variable
 fit1 <- lm(data = data.mis, Major.GPA ~ csdum + AC.CE + AE.RO)
 
 ### Model 2: plus covariates
-fit2 <- lm(Major.GPA ~ csdum + AC.CE + AE.RO + Age +
- Parents.education, data = data.mis)
+fit2 <- lm(data = data.mis, Major.GPA ~ csdum + AC.CE + AE.RO +
+  Age + Parents.education)
+
+# TODO: Run a few more lm just to make sure, but age and parents'
+# education seem like the reasonable ticket.
+# Also, run some with just AC-CE and just AE-RO.
+
+# Here's where we calculate the robust standard errors so we can
+# use them in the tables.
+cov1 <- vcovHC(fit1, type = "HC1")
+robust_se_1 <- sqrt(diag(cov1))
+cov2 <- vcovHC(fit1, type = "HC1")
+robust_se_2 <- sqrt(diag(cov2))
 
 # Let's visualize that
-stargazer(fit1, fit2)
+stargazer(fit1, fit2, se = list(robust_se_1, robust_se_2), type = "text")
 
 # Plot fit1
 df$fit1 <- stats::predict(fit1, newdata=data.mis)
@@ -158,8 +178,8 @@ err <- stats::predict(fit1, newdata=data.mis, se = TRUE)
 fit1_plot <- ggplot(df)
 fit1_plot <- fit1_plot + geom_point(aes(x=major_gpa, y = fit1),
   size = 2)
-fit1_plot <- fit1_plot + geom_smooth(data=df, aes(x=major_gpa,
-  y=fit1), size = 1.5, colour = "blue", se = TRUE,
+fit1_plot <- fit1_plot + geom_smooth(data=df, aes(x = major_gpa,
+  y = fit1), size = 1.5, colour = "blue", se = TRUE,
   stat = "smooth", method = lm)
 fit1_plot <- fit1_plot + labs(x = "Major GPA", y =
   "CS dummy variable + AC-CE + AE-RO")
@@ -185,14 +205,43 @@ print(fit2_plot, vp = viewport(layout.pos.row = 1,
 dev.off()
 
 #### Test of joint significance (linear hypothesis test)
-cs_it_lht <- lm(Major.GPA ~ csdum + AE.RO + Age +
-  Parents.education, data = data.mis)
+# We're not looking at the individual variables, so let's look
+# at them individually and see if there's a joint significance.
+# The null hypothesis is that all of these are at 0 so let's see
+# if at least one of them isn't.
+# This is checking the residual (error) sum of squares to see
+# if we can explain the deviance in the model (by running two
+# models and comparing those error sum of squares values). So not
+# including these variables (if the F statistic is high)
+# increases the amount of unexplained variance in the data.
+# So, including these variables explains more of the variance
+# than when you don't include them.
+# In reality, having one that's significant at the single level
+# will generally make you fail the null hypothesis (meaning there
+# is a joint significance to they should be included in the
+# regression). If they fail the null hypothesis,
+# then you should keep them in the regression because they
+# somehow help contribute to the model because they help
+# explain the std error (explain the deviance in the model).
+# However, if they're all significant then we'll reject the joint
+# significance null hypothesis even though none of them have a
+# p < 0.05.
+# Removing unnecessary variables will give you more power with
+# this small of a dataset.
+cs_it_lht <- lm(data = data.mis, Major.GPA ~ csdum + AE.RO +
+  Age + Parents.education)
 lht(cs_it_lht, c("AE.RO = 0",
   "Parents.educationgraduate degree = 0",
   "Parents.educationpost-graduate degree = 0",
   "Parents.educationsome college = 0",
   "Parents.educationundergraduate degree = 0"),
   white.adjust = "hc1")
+# With the p-value being so low, we confidently reject the null
+# hypothesis that these variables are not significant in
+# explaining the variance in the data.
+# I feel comfortable not including age here because it has
+# already been shown to be significant in the multiple regression
+# model.
 
 ## Question 3: How strong is the correlation between AC-CE and
 ## AE-RO, and student satisfaction in CS, IS, and IT?
